a0ebab5468766f4718242b8cfa8c36c648098fde
Raphael <raphael.araujo@estantemagica.com.br>
2020-07-04 00:11:08 -0300

    Resolução do desafio.

--- src/app.js
+++ src/app.js
@@ -1,33 +1,100 @@
-const express = require("express");
-const cors = require("cors");
+const express = require("express")
+const cors = require("cors")
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4")
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-const repositories = [];
+const repositories = []
 
 app.get("/repositories", (request, response) => {
-  // TODO
-});
+    return response.json(repositories)
+})
 
 app.post("/repositories", (request, response) => {
-  // TODO
-});
+    const { title, url, techs } = request.body
+
+    const repositorie = {
+        id: uuid(),
+        title,
+        url,
+        techs,
+        likes: 0
+    }
+
+    repositories.push(repositorie)
+
+    return response.json(repositorie)
+})
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
-});
+    const { id } = request.params
+    const { title, url, techs } = request.body
+
+    const repositorieIndex = repositories.findIndex(repositorie => {
+        return repositorie.id === id
+    })
+
+    if (repositorieIndex < 0) {
+        return response.status(400).json({
+            message: 'Repositorie not found.'
+        })
+    }
+
+    const repositorie = {
+        id,
+        title,
+        url,
+        techs,
+        likes: repositories[repositorieIndex].likes
+    }
+
+    repositories[repositorieIndex] = repositorie
+
+    return response.json(repositorie)
+})
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
+    const { id } = request.params
+
+    const repositorieIndex = repositories.findIndex(repositorie => {
+        return repositorie.id === id
+    })
+
+    if (repositorieIndex < 0) {
+        return response.status(400).json({
+            message: 'Repositorie not found.'
+        })
+    }
+
+    repositories.splice(repositorieIndex, 1)
+
+    return response.status(204).send()
+})
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
-});
+    const { id } = request.params
 
-module.exports = app;
+    const repositorieIndex = repositories.findIndex(repositorie => {
+        return repositorie.id === id
+    })
+
+    if (repositorieIndex < 0) {
+        return response.status(400).json({
+            message: 'Repositorie not found.'
+        })
+    }
+
+    const repositorie = repositories[repositorieIndex]
+
+    repositorie.likes++
+
+    repositories[repositorieIndex] = repositorie
+
+    return response.json(repositorie)
+})
+
+module.exports = app
